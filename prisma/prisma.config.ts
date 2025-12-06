31d479c1bbf449ee784956d18c6b4c2ccacfa17d
badrsa10 <smiminbadr@gmail.com>
2025-12-06 09:14:21 +0100

    06122025

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,10 +1,24 @@
-export default {
-  datasources: {
-    db: {
-      adapter: {
-        url: process.env.DATABASE_URL, // ta connexion Postgres
-      },
-      // ou accelerateUrl: process.env.ACCELERATE_URL si tu utilises Accelerate
-    },
+// export default {
+//   datasources: {
+//     db: {
+//       adapter: {
+//         url: process.env.DATABASE_URL, // ta connexion Postgres
+//       },
+//       // ou accelerateUrl: process.env.ACCELERATE_URL si tu utilises Accelerate
+//     },
+//   },
+// };
+
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config';
+
+export default defineConfig({
+  schema: './schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+    seed: `tsx prisma/seed.ts`,
   },
-};
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
+});
